5ac005bdc94173c28f057e1194ad111390059f0b
jgrojas97 <juangabrielrojas97@gmail.com>
2023-10-05 15:32:46 -0300

    AHT10

--- main.py
+++ main.py
@@ -1,53 +1,14 @@
-# Germán Andrés Xander 2023
-
-from machine import Pin, Timer
-import dht
+# AHT10 I2C test ROJAS JUan
+from machine import Pin
+from machine import I2C
+from aht10 import AHT10
 import time
-import json
-from collections import OrderedDict
-import urequests
-from settings import TOKEN, CHATID
 
-sw = Pin(23, Pin.IN)
-led = Pin(2, Pin.OUT)
-d = dht.DHT22(Pin(25))
+i2c = I2C(scl=Pin(22), sda=Pin(23), freq=400000)  
+aht10 = AHT10(i2c,0,0x38)
 
-print("esperand pulsador")
-contador=0
-estado=False
-
-def alternar(pin):
-    global contador, estado
-    if sw.value():
-        if not estado:
-            contador+=1
-            print(contador)
-            led.value(not led.value())
-            try:
-                data = {'chat_id': CHATID, 'text': datos}
-                response = urequests.post("https://api.telegram.org/bot" + TOKEN + '/sendMessage', json=data)
-                # print(response.text)
-                response.close()
-                print("envio correcto a telegram")
-            except:
-                print("fallo en el envio a telegram")
-            estado = True
-        else:
-            estado = False
-
-timer1 = Timer(1)
-timer1.init(period=50, mode=Timer.PERIODIC, callback=alternar)
+print("Inicializado")
 
 while True:
-    try:
-        d.measure()
-        temperatura=d.temperature()
-        humedad=d.humidity()
-        datos=json.dumps(OrderedDict([
-            ('temperatura',temperatura),
-            ('humedad',humedad)
-        ]))
-        print(datos)
-    except OSError as e:
-        print("sin sensor")
-    time.sleep(5)
+    aht10.print()
+    time.sleep_ms(2000)
