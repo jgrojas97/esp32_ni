e249564a75eb1276e506062e04c3e72233746bac
jgrojas97 <juangabrielrojas97@gmail.com>
2023-10-26 03:54:39 -0300

    Desarrollo Ejercicio

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ led = Pin(2, Pin.OUT)
 i2c = I2C(scl=Pin(21), sda=Pin(22), freq=400000)  
 d = AHT10(i2c,0,0x38)
 
+HIGH_THRESHOLD = 35
+LOW_THRESHOLD = 30
+ALARM_FLAG = False
+
+
 contador = 0
 
 def heartbeat(nada):
@@ -37,8 +42,8 @@ def transmitir(pin):
     mqtt.disconnect()
     pulsos.init(period=150, mode=Timer.PERIODIC, callback=heartbeat)
 
-publicar = Timer(0)
-publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
+#publicar = Timer(0)
+#publicar.init(period=30000, mode=Timer.PERIODIC, callback=transmitir)
 pulsos = Timer(1)
 
 while True:
@@ -51,6 +56,13 @@ while True:
             ('humedad',humedad)
         ]))
         print(datos)
+        if temperatura > HIGH_THRESHOLD and not ALARM_FLAG:  # Si la temperatura sube por encima de un límite superior y aún no se transmitió nada, transmite
+            transmitir()
+            ALARM_FLAG = True  # La bandera impide retransmitir los datos si la temperatura no baja por debajo del límite inferior
+
+        if temperatura < LOW_THRESHOLD:
+            ALARM_FLAG = False  # Cuando la temperatura decae por debajo el límite inferior, se desactiva la bandera
+
     except OSError as e:
         print("sin sensor")
     time.sleep(5)
